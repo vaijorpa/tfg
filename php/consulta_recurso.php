e29f10de5782cc3901b551d2e1d6d3f8e81e0ee6
vaijorpa <67892341+vaijorpa@users.noreply.github.com>
2020-07-09 10:35:22 +0200

    Add files via upload

---
+++ php/consulta_recurso.php
@@ -0,0 +1,29 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: PUT, GET, POST, DELETE");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+include('conexion.php');
+
+$response = array();
+
+$sql= "SELECT * FROM recurso";
+
+
+if ($resultado = mysqli_query($mysqli, $sql)) 
+{	
+	while($row = mysqli_fetch_array($resultado)){
+		$recurso = array();
+		$recurso["id_recurso"]=$row["id_recurso"];
+		$recurso["nombre"]=$row["nombre"];
+		$recurso["tipo_recurso"]=$row["tipo_recurso"];
+		$recurso["cantidad"]=$row["cantidad"];
+
+		array_push($response,$recurso);
+    }
+	$resultado->close();
+}
+ $mysqli->close();
+ echo json_encode($response);
+ 
+?>
